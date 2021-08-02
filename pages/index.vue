a04a2f464895062b5fa52d41252041d4fe8b83b0
Mattia Sinisi <mattia@snisni.it>
2021-08-02 22:52:41 +0200

    Removed starter template

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,16 @@
+
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+  <div class="hero is-dark is-fullheight banner">
+    <div class="hero-body container">
+      <div class="has-text-centered">
+        <h1 class="title is-0">
+          <span>Experimental Players</span>
+          <small class="has-text-primary has-text-bold"><sup>HUB</sup></small>
+        </h1>
+        <p class="subtitle is-4">
+          ...is being created.
+        </p>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
-
-<script>
-import Card from '~/components/Card'
-
-export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
-}
-</script>
