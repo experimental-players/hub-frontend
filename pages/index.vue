6e3ce1d8b132a9673b912d3cb8dbf2c43d30f32b
Mattia Sinisi <mattia@snisni.it>
2021-08-02 23:10:08 +0200

    Created main banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 
 <template>
-  <div class="hero is-dark is-fullheight banner">
-    <div class="hero-body container">
-      <div class="has-text-centered">
-        <h1 class="title is-0">
-          <span>Experimental Players</span>
-          <small class="has-text-primary has-text-bold"><sup>HUB</sup></small>
-        </h1>
-        <p class="subtitle is-4">
-          ...is being created.
-        </p>
+  <div>
+    <div class="hero is-dark is-bold is-medium banner">
+      <div class="hero-body container">
+        <div class="has-text-centered">
+          <h1 class="title is-0">
+            <span>Experimental Players</span>
+            <small class="has-text-primary has-text-bold"><sup>HUB</sup></small>
+          </h1>
+        </div>
       </div>
     </div>
   </div>
