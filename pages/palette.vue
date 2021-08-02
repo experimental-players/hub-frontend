80d6e2fe7b3d57173912a5d208db5cc464276ee4
Mattia Sinisi <mattia@snisni.it>
2021-08-02 23:09:58 +0200

    Customize and preview color palette

---
+++ pages/palette.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div>
+    <div class="hero is-medium banner">
+      <div class="hero-body container">
+        <div class="has-text-centered">
+          <h1 class="title is-1">
+            <span>Color Palette Test</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns is-vcentered">
+      <div v-for="type in types" :key="type" class="column hero is-bold" :class="'is-' + type">
+        <div class="hero-body container">
+          <p>
+            {{ type }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      types: ['primary', 'info', 'link', 'danger', 'warning', 'success', 'light', 'dark']
+    }
+  }
+}
+</script>
