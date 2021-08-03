091b5e3386a261bbfb9b5a18c9fdba2aaaf43ab5
Mattia Sinisi <mattia@snisni.it>
2021-08-03 15:22:14 +0200

    Get section from dummy store

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,9 @@
       <div class="hero-body">
         <div class="container">
           <BubbleSection
-            v-for="(item, n) in items"
+            v-for="(section, n) in everySection"
             :key="n"
-            v-bind="item"
+            v-bind="section"
           />
         </div>
       </div>
@@ -27,34 +27,11 @@
 </template>
 
 <script>
-import Section from '@/models/Section'
+import { mapGetters } from 'vuex'
 
 export default {
-  data () {
-    return {
-      items: [
-        {
-          ...new Section('Minecraft', 'https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png', 'https://cdnb.artstation.com/p/assets/images/images/013/535/601/large/supawit-oat-fin1.jpg?1540053395', '#45fd45'),
-          glow: true
-        },
-        {
-          title: 'Discord',
-          icon: 'https://www.freepnglogos.com/uploads/discord-logo-png/discord-icon-flat-style-available-svg-png-eps-10.png',
-          image: 'https://i.pinimg.com/originals/fd/91/13/fd91131ea693096d6be5e8aa99d18f9e.jpg',
-          color: '#7289da',
-          glow: true
-        },
-        {
-          title: 'GitHub',
-          icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
-          image: 'https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z2l0aHVifGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
-          color: 'black'
-        },
-        {
-          title: 'Empty'
-        }
-      ]
-    }
+  computed: {
+    ...mapGetters('sections', ['everySection'])
   }
 }
 </script>
