6943b7849dbc8c6c2c2a28f3bb438a29767d5e0b
Mattia Sinisi <mattia@snisni.it>
2021-08-03 14:28:38 +0200

    WIP: Created section bubble component with dynamic colors

---
+++ components/BubbleSection.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="main box is-block section" :style="boxStyle">
+    <div class="columns is-vcentered">
+      <div class="column is-one-fifth">
+        <figure class="box is-inline-block has-shadow">
+          <p class="image is-64x64">
+            <img :src="icon">
+          </p>
+        </figure>
+      </div>
+
+      <div class="column">
+        <div class="content">
+          <p class="title is-1" :style="textStyle" v-text="title" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Color from 'chroma-js'
+
+export default {
+  props: {
+    icon: String,
+    image: String,
+    color: String,
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    boxStyle () {
+      return `background: linear-gradient(to right, ${this.backgroundColor} 30%, ${this.colorEntity.alpha(0.5)}), url(${this.image});`
+    },
+    textStyle () {
+      return `color: ${this.foregroundColor};`
+    },
+    colorEntity () {
+      return Color(this.color)
+    },
+    backgroundColor () {
+      return this.colorEntity.hex()
+    },
+    foregroundColor () {
+      if (Color.distance(this.backgroundColor, 'black') > 50) {
+        return 'black'
+      }
+      return 'white'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main,
+.box {
+  border-radius: 1em;
+}
+
+figure.box {
+  background-color: white;
+}
+
+.main {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: right center;
+}
+</style>
