24d2d325458190fa27c6e0a159f8a8748a91b1e0
Mattia Sinisi <mattia@snisni.it>
2021-08-03 17:46:47 +0200

    Removed superfluos container

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <div class="hero is-medium banner" :style="bannerStyle">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-0" :style="bannerForegroundStyle" v-text="title" />
-        </div>
+  <div class="hero is-medium banner" :style="bannerStyle">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-0" :style="bannerForegroundStyle" v-text="title" />
       </div>
     </div>
   </div>
