190aea0d350031c5d16caf6d808c986a65037d30
Mattia Sinisi <mattia@snisni.it>
2021-08-03 15:37:24 +0200

    WIP: Created section page

---
+++ pages/section/_section.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="hero is-dark is-medium banner" :style="bannerStyle">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-0" v-text="section.title" />
+        </div>
+      </div>
+    </div>
+
+    <div class="hero is-dark is-bold">
+      <div class="hero-body">
+        <div class="container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus eveniet molestiae totam magni similique iste vel dolore perspiciatis assumenda. Harum doloribus expedita ipsa ipsam commodi fugit eos officia dicta eaque?
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    section () {
+      return this.$store.state.sections.list.filter(section => section.title === this.$route.params.section)[0] ?? null
+    },
+    bannerStyle () {
+      return {
+        background: `linear-gradient(to right, black 30%, rgba(0, 0, 0, .1)), url(${this.section.image})`
+      }
+    }
+  }
+}
+</script>
