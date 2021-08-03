34a78a21bc1699b336e35af508f3bb28c73350f3
Mattia Sinisi <mattia@snisni.it>
2021-08-03 18:24:02 +0200

    Removed comments

--- models/Section.ts
+++ models/Section.ts
@@ -11,12 +11,3 @@ export default class Section {
     this.color = color;
   }
 }
-
-/*
-{
-          title: 'GitHub',
-          icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png',
-          image: 'https://images.unsplash.com/photo-1556075798-4825dfaaf498?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z2l0aHVifGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
-          color: 'black'
-        },
-*/
