88db85869a17b6b2cc519c6276035c9995a1c8c4
Mattia Sinisi <mattia@snisni.it>
2021-08-04 22:32:43 +0200

    Re-styled section page

--- pages/section/_section.vue
+++ pages/section/_section.vue
@@ -1,51 +1,53 @@
 <template>
-  <div>
-    <Banner v-bind="section" />
+  <div class="has-background-dark">
+    <Banner v-bind="section" :glow="false" class="pb-intersection clipped-banner" />
 
-    <div class="hero is-dark is-bold is-small">
-      <div class="hero-body">
-        <div class="container">
-          <p class="subtitle is-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nobis facere minima consequatur, odio repellendus vitae suscipit unde, soluta reiciendis ad id dolor aperiam omnis veniam esse, quasi nihil magnam.
-          </p>
+    <div class="container is-rounded mt-negative-intersection pb-medium">
+      <div class="section is-rounded hero is-dark is-bold is-small description-box">
+        <div class="hero-body">
+          <div class="container">
+            <p class="subtitle is-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nobis facere minima consequatur, odio repellendus vitae suscipit unde, soluta reiciendis ad id dolor aperiam omnis veniam esse, quasi nihil magnam.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="pb-medium">
-      <div class="container section content">
-        <!-- EXAMPLE CONTENT -->
-        <h1 id="sample-markdown">
-          Sample Markdown
-        </h1>
-        <p>This is some basic, sample markdown.</p>
-        <h2 id="second-heading">
-          Second Heading
-        </h2>
-        <ul>
-          <li>
-            Unordered lists, and:
-            <ol>
-              <li>One</li>
-              <li>Two</li>
-              <li>Three</li>
-            </ol>
-          </li>
-          <li>More</li>
-        </ul>
-        <blockquote>
-          <p>Blockquote</p>
-        </blockquote>
-        <p>
-          And <strong>bold</strong>, <em>italics</em>, and even
-          <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>.
-          <a href="https://markdowntohtml.com">A link</a> to somewhere.
-        </p>
-        <p>And code highlighting:</p>
-        <p>Or inline code like <code>var foo = &#39;bar&#39;;</code>.</p>
-        <p>Or an image of bears</p>
-        <p><img src="http://placebear.com/200/200" alt="bears"></p>
-        <p>The end ...</p>
+      <div class="is-rounded has-background-light main-page-content">
+        <div class="section content">
+          <!-- EXAMPLE CONTENT -->
+          <h1 id="sample-markdown">
+            Sample Markdown
+          </h1>
+          <p>This is some basic, sample markdown.</p>
+          <h2 id="second-heading">
+            Second Heading
+          </h2>
+          <ul>
+            <li>
+              Unordered lists, and:
+              <ol>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+              </ol>
+            </li>
+            <li>More</li>
+          </ul>
+          <blockquote>
+            <p>Blockquote</p>
+          </blockquote>
+          <p>
+            And <strong>bold</strong>, <em>italics</em>, and even
+            <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>.
+            <a href="https://markdowntohtml.com">A link</a> to somewhere.
+          </p>
+          <p>And code highlighting:</p>
+          <p>Or inline code like <code>var foo = &#39;bar&#39;;</code>.</p>
+          <p>Or an image of bears</p>
+          <p><img src="http://placebear.com/200/200" alt="bears"></p>
+          <p>The end ...</p>
+        </div>
       </div>
     </div>
   </div>
@@ -64,3 +66,19 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.clipped-banner {
+  clip-path: polygon(0 0, 100% 0, 100% 80%, 0% 100%);
+}
+
+.description-box {
+  border-bottom-right-radius: 0 !important;
+  border-bottom-left-radius: 0 !important;
+}
+
+.main-page-content {
+  border-top-right-radius: 0 !important;
+  border-top-left-radius: 0 !important;
+}
+</style>
