069fb6bfa84ae50dd9e65591cd0f3092700ce49e
Mattia Sinisi <mattia@snisni.it>
2021-08-04 22:01:41 +0200

    Added FIXME

--- store/sections.js
+++ store/sections.js
@@ -1,6 +1,8 @@
 import Section from '@/models/Section';
 import Link from '@/models/Link';
 
+// FIXME: https://github.com/vuex-orm/vuex-orm/issues/255
+
 export const state = () => ({
   list: [
     {
