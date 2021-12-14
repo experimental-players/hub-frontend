b9342ff2b89ab6ccadcd7ebc59c4f43d338a2653
Mattia Sinisi <mattia.sinisi@virgilio.it>
2021-12-14 11:37:31 +0100

    Updated projects module

--- store/projects.ts
+++ store/projects.ts
@@ -1,16 +1,16 @@
-// import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
+import { Module, VuexModule, Mutation } from 'vuex-module-decorators';
 
-// import Project from '~/models/Project';
+import Project from '~/models/Project';
 
-// @Module({
-//   name: 'projects',
-//   stateFactory: true,
-//   namespaced: true
-// })
-// class ProjectsModule extends VuexModule {
-//   private projects: Project[] = [];
+@Module({
+  name: 'projects',
+  stateFactory: true,
+  namespaced: true
+})
+class ProjectsModule extends VuexModule {
+  private projects: Project[] = [];
 
-//   get projects() {
-//     return this.projects
-//   }
-// }
+  get all() {
+    return this.projects
+  }
+}
