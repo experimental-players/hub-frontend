efed8033c07fa94b31273ac8beac22f118b26155
Mattia Sinisi <mattia@snisni.it>
2021-12-20 18:18:46 +0100

    Mapped Project class

--- models/Project.ts
+++ models/Project.ts
@@ -2,5 +2,13 @@ import { BuildableResource, Properties } from 'tapi.js';
 
 @Properties.Resource
 export default class Project extends BuildableResource {
+  public id: string = '';
 
+  @Properties.Alias('codename')
+  public code: string = '';
+
+  @Properties.Alias('fullname')
+  public name: string = '';
+
+  public description: string = '';
 }
