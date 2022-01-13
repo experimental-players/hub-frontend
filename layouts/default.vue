2ee1676198a425914b71d21d09e4a8ce0c8a3b28
Mattia Sinisi <mattia@snisni.it>
2022-01-13 23:28:12 +0100

    Added basic navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,41 @@
 <template>
-  <Nuxt />
+  <div>
+    <nav class="navbar is-primary is-fixed-bottom" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link class="navbar-item" to="/">
+          <span v-if="isRoot" class="has-text-weight-bold">
+            exp//hub
+          </span>
+
+          <span v-else>
+            &lt;-- back
+          </span>
+        </nuxt-link>
+      </div>
+    </nav>
+
+    <div>
+      <Nuxt />
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    isRoot () {
+      return this.$route.path === '/'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  font-family: monospace;
+
+  &, .navbar-item {
+    border-radius: 20px 0 0 0 !important;
+  }
+}
+</style>
